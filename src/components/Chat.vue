fd4e93f1bb37677f86f359f53170f44d3e10c44c
mystermiam <stephane.jabol@gmail.com>
2018-05-22 23:51:23 +0200

    chat display enhanced a bit

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="chatContainer">
     <div id="chatBox" class="chatBox">
-        <message v-for="(message,i) in messages" :key="message.time+message.nickmame" :message="message" :previous="i>0 ? messages[i-1] : ''">
+        <message v-for="(message,i) in messages" :key="i" :message="message" :previous="i>0 ? messages[i-1] : ''">
         </message>
     </div>
     <div>
-        <input type="text" name="username" v-model="nickmame"><br>
-        <input v-model="content" type="text" name="typeBar"/><button @click="sendMessage">Send</button>
-   </div>
+      <div class="inputContainer">
+        
+          <input type="text" name="username" placeholder="Who are you ?" v-model="nickname">
+          <textarea id="textField" v-model="textMessage" @keydown="handleKey" placeholder="Say whatever..." rows=1></textarea>
+        
+     </div>
+    </div>
   </div>
 </template>
 <!-- computed: {
@@ -33,42 +37,42 @@ export default {
   },
   data(){
     return {
-        nickmame : '',
-        content : ''
+        nickname : '',
+        textMessage : ''
     };
   },
   computed : {
     ...mapState('chat',{
       messages:'messages'
-    }),
-    nickname : {
-        get(){
-            return this.nickmame;
-        },
-        set(nickmame){
-            this.nickname = nickname;
-        }
-    },
-    content : {
-        get(){
-            return this.content;
-        },
-        set(content){
-            this.content = content;
-        }
-    }
+    })
   },
   methods : {
     ...mapActions('chat',{
       concatMessages:'concatMessages'
     }),
-    sendMessage(){
-      if(this.nickname && this.content)
-        this.concatMessages([{
-            nickname: this.nickname,
-            time : new Date(),
-            content : this.content 
-        }]);
+    handleKey(e){
+
+      if(e.shiftKey && e.keyCode == 13){
+        if(this.$el.querySelector('#textField').rows < 13) 
+          this.$el.querySelector('#textField').rows++;
+      } else if(e.keyCode == 8 && this.$el.querySelector('#textField').rows > 1){
+        let lines = this.$el.querySelector('#textField').value.split('\n');
+
+        if(!lines[lines.length - 1]) 
+            this.$el.querySelector('#textField').rows--;
+        
+      } else if(e.keyCode == 13){
+        e.preventDefault();
+        if(this.nickname && this.textMessage)
+          this.concatMessages([{
+              nickname: this.nickname,
+              time : new Date(),
+              textMessage : this.textMessage 
+          }]);
+        console.log('tm '+ this.textMessage);
+        this.textMessage='';
+        this.$el.querySelector('#textField').rows = 1;
+      }
     }
   },
   updated(){
@@ -79,6 +83,5 @@ export default {
 </script>
 
 <style scoped>
-
 
 <style>
