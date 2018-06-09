a412e565aba4d1ed0a98f160a276b3e26843e89a
Pascal Kolbe <pascalkolbe@web.de>
2018-06-09 22:50:31 +0200

    Success! Character body created! :)

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -30,6 +30,15 @@ const webpackConfig = merge(baseWebpackConfig, {
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
   plugins: [
+
+    new webpack.ProvidePlugin({
+    $: 'jquery',
+    jquery: 'jquery',
+    'window.jQuery': 'jquery',
+    jQuery: 'jquery'
+    }),
+
+
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
       'process.env': env
