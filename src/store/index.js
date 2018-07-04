524eef71fbcabae8b50c04097292f3bfe53fb385
mystermiam <stephane.jabol@gmail.com>
2018-07-04 16:11:49 +0200

    server settings 2

--- src/store/index.js
+++ src/store/index.js
@@ -12,7 +12,6 @@ import chat from './modules/pomodoro/chat'
 import session from './modules/pomodoro/session'
 import distractionList from './modules/pomodoro/distractionList'
 import sessionTitleList from './modules/pomodoro/sessionTitleList'
-import feedback from './modules/pomodoro/feedback'
 
 
 //Sidebar Modules
@@ -26,6 +25,7 @@ import dailies from './modules/menu/toolbox/dailies'
 import landingPage from './modules/homepage/landingPage'
 import axios from 'axios'
 
+
 Vue.use(Vuex)
 Vue.use(VueRouter)
 
@@ -38,18 +38,17 @@ export default new Vuex.Store({
         distractionList,
         sessionTitleList,
         session,
-        feedback,
         avatar,
         pets,
         landingPage,
         dailies,
     },
     state : {
-        userIsLoggedIn : true
+        userIsLoggedIn : false
     },
     actions : {
         checkCredentials(context,user){
-            axios.post('http://localhost:3801/api/connection',user).then(function(response){
+            axios.post('http://grow.cri-paris.org/api/connection',user).then(function(response){
                 if(response.data.error){
                   console.log('not granted')
                 }
