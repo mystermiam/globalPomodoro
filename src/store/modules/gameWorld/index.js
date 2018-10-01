57fc7ea11c1f5b33c8c81581db3482a404fec254
Pascal Kolbe <pascalkolbe@web.de>
2018-10-01 17:33:06 +0200

    Merge branch 'master' of https://github.com/mystermiam/globalPomodoro

--- src/store/modules/gameWorld/index.js
+++ src/store/modules/gameWorld/index.js
@@ -14,6 +14,7 @@ import NuitBlancheTown from './scenes/NuitBlancheTown'
 // DEMO SCENES
 import Bedroom from './scenes/Demo/Bedroom'
 import LivingRoom from './scenes/Demo/LivingRoom'
+import Town from './scenes/Demo/Town'
 
 var Grow = [];
 
@@ -31,7 +32,7 @@ function launch() {
         debug: false
       }
     },
-    scene: [BootScene, Bedroom, LivingRoom]
+    scene: [BootScene, Bedroom, LivingRoom, Town]
     //scene: [BootScene, HouseOfMusicScene, TownScene, EmptyGrassField, NuitBlancheTown] // this defines, which number the scene has: Grow.scene.scenes[2].player;
 
     //scene: [BootScene, PlayScene]
